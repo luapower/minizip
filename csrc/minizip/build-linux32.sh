eabb01afdf3f62c721acc59565c16cfb24dc71a1
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2013-12-24 16:44:14 +0200

    changed binary paths in build scripts

--- csrc/minizip/build-linux32.sh
+++ csrc/minizip/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc zip.c unzip.c ioapi.c -O3 -s -shared -o ../../linux/bin/libminizip.so -I. -I../zlib -L../../linux/bin -lz
+gcc zip.c unzip.c ioapi.c -O3 -s -shared -o ../../bin/linux32/libminizip.so -I. -I../zlib -L../../bin/linux32 -lz
 
-cd ../.. && linux/bin/luajit minizip_test.lua
+#cd ../.. && linux/bin/luajit minizip_test.lua
