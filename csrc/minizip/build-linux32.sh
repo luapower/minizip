241f5f753b488dad660f9f8fa9228bf61baf36e9
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-14 14:41:21 +0200

    added $CFLAGS option and recompiled with -O2

--- csrc/minizip/build-linux32.sh
+++ csrc/minizip/build-linux32.sh
@@ -1,3 +1,3 @@
-gcc zip.c unzip.c ioapi.c -O3 -s -shared -o ../../bin/linux32/libminizip.so -I. -I../zlib -L../../bin/linux32 -lz
+gcc $CFLAGS zip.c unzip.c ioapi.c -shared -o ../../bin/linux32/libminizip.so -I. -I../zlib -L../../bin/linux32 -lz
 
 #cd ../.. && linux/bin/luajit minizip_test.lua
--- csrc/minizip/build-mingw32.sh
+++ csrc/minizip/build-mingw32.sh
@@ -1,3 +1,3 @@
-gcc zip.c unzip.c ioapi.c iowin32.c -O3 -s -shared -o ../../bin/mingw32/minizip.dll -I. -I../zlib -L../../bin/mingw32 -lz
+gcc $CFLAGS zip.c unzip.c ioapi.c iowin32.c -shared -o ../../bin/mingw32/minizip.dll -I. -I../zlib -L../../bin/mingw32 -lz
 
 #cd ../.. && bin/luajit minizip_test.lua
