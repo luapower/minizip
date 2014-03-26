8b85d450ca903b826ebfa4fbd346c8524abdbad2
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:08:34 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/minizip/build-osx32.sh
+++ csrc/minizip/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 zip.c unzip.c ioapi.c -shared -o ../../bin/osx32/libminizip.dylib -I. -I../zlib -L../../bin/osx32 -lz
+gcc -arch i386 -O2 zip.c unzip.c ioapi.c -shared -install_name @loader_path/libminizip.dylib -o ../../bin/osx32/libminizip.dylib -I. -I../zlib -L../../bin/osx32 -lz
--- csrc/minizip/build-osx64.sh
+++ csrc/minizip/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 zip.c unzip.c ioapi.c -shared -o ../../bin/osx64/libminizip.dylib -I. -I../zlib -L../../bin/osx64 -lz
+gcc -arch x86_64 -O2 zip.c unzip.c ioapi.c -shared -install_name @loader_path/libminizip.dylib -o ../../bin/osx64/libminizip.dylib -I. -I../zlib -L../../bin/osx64 -lz
