cd32b5192cfca7ec0889535ee6862bf9bbf51b3a
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-11-03 12:48:08 +0000

    unimportant

--- csrc/minizip/build.sh
+++ csrc/minizip/build.sh
@@ -1,4 +1,4 @@
-gcc -c -O2 $C zip.c unzip.c ioapi.c -I. -I../zlib
-gcc *.o -shared -o ../../bin/$P/$D -L../../bin/$P -lz $L
-ar rcs ../../bin/$P/$A *.o
+${X}gcc -c -O2 $C zip.c unzip.c ioapi.c -I. -I../zlib
+${X}gcc *.o -shared -o ../../bin/$P/$D -L../../bin/$P -lz $L
+${X}ar rcs ../../bin/$P/$A *.o
 rm *.o
